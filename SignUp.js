d11303e3f7365bb66390500975404f7ec9ae6f0e
AbdulRehman <ab.github786@gmail.com>
2024-11-20 15:10:42 +0500

    Initial commit

---
+++ SignUp.js
@@ -0,0 +1,19 @@
+// script.js
+const loginTab = document.getElementById("login-tab");
+const signupTab = document.getElementById("signup-tab");
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+
+loginTab.addEventListener("click", () => {
+  loginTab.classList.add("active");
+  signupTab.classList.remove("active");
+  loginForm.classList.remove("hidden");
+  signupForm.classList.add("hidden");
+});
+
+signupTab.addEventListener("click", () => {
+  signupTab.classList.add("active");
+  loginTab.classList.remove("active");
+  signupForm.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+});
